235a5926c869603e61c6e2b8ed469843f5abc1c2
Nicola <fatepepe66@gmail.com>
2024-09-21 17:46:50 +0200

    bug  fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
 import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import {useRef}  from 'react'
+import Navbar from "./sections/Navbar"
 
 
-const RotatingCube = () => {
+// const RotatingCube = () => {
 
-  const meshRef = useRef()
+//   const meshRef = useRef()
 
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.y += 0.01
-      meshRef.current.rotation.x +=  0.01
-    }
-  })
-    return (
-      <mesh ref={meshRef}>
-         <cylinderGeometry args={[1,1,1]} />
-         <meshLambertMaterial color="#468585" emissive="3468585" />
-      </mesh>
-    )
-}
+//   useFrame(() => {
+//     if (meshRef.current) {
+//       meshRef.current.rotation.y += 0.01
+//       meshRef.current.rotation.x +=  0.01
+//     }
+//   })
+//     return (
+//       <mesh ref={meshRef}>
+//          <cylinderGeometry args={[1,1,1]} />
+//          <meshLambertMaterial color="#468585" emissive="3468585" />
+//       </mesh>
+//     )
+// }
 
 
 const App = () => {
   return (
-     
-       
-
-          <div className="bg-white c-space  transition-colors text-red-900">hello world </div>
-        
-     
+      <Navbar />
   )
 }
 
