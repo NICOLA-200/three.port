efa496ae8c55e54581933327eb1f9fb545885541
Nicola <fatepepe66@gmail.com>
2024-09-23 16:16:22 +0200

    some chagnes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        generalsans: ['General Sans', 'sans-serif'],
-      },
-    },
-  },
-  plugins: [],
-}/** @type {import('tailwindcss').Config} */
-export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
