6cb56eacd73f44a394fd070d9a0abb16f80fe52d
Nicola <fatepepe66@gmail.com>
2024-09-23 15:36:54 +0200

    tailwind updted

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 import {useRef}  from 'react'
 import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero"
 
 
 // const RotatingCube = () => {
@@ -25,7 +26,10 @@ import Navbar from "./sections/Navbar"
 
 const App = () => {
   return (
+    <main className="max-w-7xl mx-auto relative">
       <Navbar />
+      <Hero />
+      </main>
   )
 }
 
