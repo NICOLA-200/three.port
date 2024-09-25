a26e4e2f82702216bc69aecbec258fafa5f683e5
Nicola <fatepepe66@gmail.com>
2024-09-25 08:40:48 +0200

    changes

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
       .send(
         // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
         'service_wemzu0a',
-        'template_blmmtcn',
+        'template_h5x75zw',
         // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
